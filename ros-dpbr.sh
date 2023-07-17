c26d074a99fe2621f162fa393560cbfd1e0e8efd
zhongyb <zhongyb@gmail.com>
2023-07-17 19:33:46 +0800

    Update ros-dpbr.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -9,7 +9,7 @@ wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/mayax
 echo "/ip firewall address-list"
 
 for net in $(cat CN.txt) ; do
-  echo "add list=CN address=$net comment=AS4809"
+  echo "add list=China_IP address=$net comment=AS4809"
 done
 
 } > ../CN.rsc
